f2f466862e0226118fdb65006437eecaeb28bf7f
Isaias Pomales <impomales@gmail.com>
2018-10-01 19:11:01 -0400

    add header and remove default styling

--- App.js
+++ App.js
@@ -1,26 +1,18 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView } from 'react-native';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import reducers from './src/reducers';
+import { Header } from './src/components/common';
 
 export default class App extends React.Component {
   render() {
     return (
       <Provider store={createStore(reducers)}>
-        <View style={styles.container}>
-          <Text>Open up App.js to start working on your app!</Text>
-        </View>
+        <SafeAreaView>
+          <Header headerText="Tech Stack" />
+        </SafeAreaView>
       </Provider>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center'
-  }
-});
