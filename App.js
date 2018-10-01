93d9e10d099ea8edf98f14c1c6e59816492d79f0
Isaias Pomales <impomales@gmail.com>
2018-10-01 19:12:53 -0400

    destructure Component

--- App.js
+++ App.js
@@ -1,11 +1,11 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { SafeAreaView } from 'react-native';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import reducers from './src/reducers';
 import { Header } from './src/components/common';
 
-export default class App extends React.Component {
+export default class App extends Component {
   render() {
     return (
       <Provider store={createStore(reducers)}>
