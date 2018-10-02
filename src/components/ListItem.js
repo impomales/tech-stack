680d45fb7ca2a2dd99369598a3f8b06b82c378d4
Isaias Pomales <impomales@gmail.com>
2018-10-02 14:16:49 -0400

    style library titles

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,12 +1,20 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { Text, StyleSheet } from 'react-native';
 import { CardSection } from './common';
+
+const styles = StyleSheet.create({
+  titleStyle: {
+    fontSize: 18,
+    paddingLeft: 15
+  }
+});
 
 const ListItem = ({ library }) => {
   return (
-  <CardSection>
-    <Text>{library.item.title}</Text>
-  </CardSection>
-)};
+    <CardSection>
+      <Text style={styles.titleStyle}>{library.item.title}</Text>
+    </CardSection>
+  );
+};
 
 export default ListItem;
